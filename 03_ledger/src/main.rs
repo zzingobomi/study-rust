e3fe82b1aa61fa30e62bb37709ecc96c0fcdaa59
zzingo <zzingo5@gmail.com>
2026-08-16 13:19:23 +0900

    feat: legder 구현

--- 03_ledger/src/main.rs
+++ 03_ledger/src/main.rs
@@ -1,4 +1,8 @@
-use clap::{Parser, Subcommand};
+use chrono::NaiveDate;
+use clap::{Args, Parser, Subcommand};
+use csv::{Reader, Writer, WriterBuilder};
+use serde::{Deserialize, Serialize};
+use std::{collections::HashMap, fs::OpenOptions};
 
 #[derive(Parser)]
 #[clap(version = "1.0")]
@@ -9,17 +13,135 @@ struct App {
 #[derive(Subcommand)]
 enum Command {
     /// 신규 계좌 작성
-    New,
+    New(NewArgs),
     /// 계좌 입금
-    Deposit,
+    Deposit(DepositArgs),
     /// 계좌 출금
-    Withdraw,
+    Withdraw(WithdrawArgs),
     /// CSV에서 가져오기
-    Import,
+    Import(ImportArgs),
     /// 리포트 작성
-    Report,
+    Report(ReportArgs),
+}
+#[derive(Args)]
+struct NewArgs {
+    account_name: String,
+}
+impl NewArgs {
+    fn run(&self) {
+        let file_name = format!("{}.csv", self.account_name);
+        let mut writer = Writer::from_path(file_name).unwrap();
+        writer.write_record(["날짜", "용도", "금액"]).unwrap();
+        writer.flush().unwrap();
+    }
+}
+#[derive(Args)]
+struct DepositArgs {
+    account_name: String,
+    date: NaiveDate,
+    usage: String,
+    amount: u32,
+}
+impl DepositArgs {
+    fn run(&self) {
+        let open_option = OpenOptions::new()
+            .write(true)
+            .append(true)
+            .open(format!("{}.csv", self.account_name))
+            .unwrap();
+        let mut writer = Writer::from_writer(open_option);
+        writer
+            .write_record(&[
+                self.date.format("%Y-%m-%d").to_string(),
+                self.usage.to_string(),
+                self.amount.to_string(),
+            ])
+            .unwrap();
+        writer.flush().unwrap();
+    }
+}
+#[derive(Args)]
+struct WithdrawArgs {
+    account_name: String,
+    date: NaiveDate,
+    usage: String,
+    amount: u32,
+}
+impl WithdrawArgs {
+    fn run(&self) {
+        let open_option = OpenOptions::new()
+            .write(true)
+            .append(true)
+            .open(format!("{}.csv", self.account_name))
+            .unwrap();
+        let mut writer = Writer::from_writer(open_option);
+        writer
+            .write_record(&[
+                self.date.format("%Y-%m-%d").to_string(),
+                self.usage.to_string(),
+                format!("-{}", self.amount),
+            ])
+            .unwrap();
+        writer.flush().unwrap();
+    }
+}
+#[derive(Args)]
+struct ImportArgs {
+    src_file_name: String,
+    dst_account_name: String,
+}
+impl ImportArgs {
+    fn run(&self) {
+        let open_option = OpenOptions::new()
+            .write(true)
+            .append(true)
+            .open(format!("{}.csv", self.dst_account_name))
+            .unwrap();
+        let mut writer = WriterBuilder::new()
+            .has_headers(false)
+            .from_writer(open_option);
+        let mut reader = Reader::from_path(&self.src_file_name).unwrap();
+        for result in reader.deserialize() {
+            let record: Record = result.unwrap();
+            writer.serialize(record).unwrap();
+        }
+        writer.flush().unwrap();
+    }
+}
+#[derive(Serialize, Deserialize)]
+struct Record {
+    날짜: NaiveDate,
+    용도: String,
+    금액: i32,
+}
+#[derive(Args)]
+struct ReportArgs {
+    files: Vec<String>,
+}
+impl ReportArgs {
+    fn run(&self) {
+        let mut map = HashMap::new();
+        for file in &self.files {
+            let mut reader = Reader::from_path(file).unwrap();
+            for result in reader.deserialize() {
+                let record: Record = result.unwrap();
+                let sum = map
+                    .entry(record.날짜.format("%Y-%m").to_string())
+                    .or_insert(0);
+                *sum += record.금액;
+            }
+        }
+        println!("{:?}", map);
+    }
 }
 
 fn main() {
-    let _args = App::parse();
+    let args = App::parse();
+    match args.command {
+        Command::New(args) => args.run(),
+        Command::Deposit(args) => args.run(),
+        Command::Withdraw(args) => args.run(),
+        Command::Import(args) => args.run(),
+        Command::Report(args) => args.run(),
+    }
 }
