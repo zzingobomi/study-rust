3a930d232a6ccc3cf8990f69dd34d5ce46c55ce9
zzingo5 <zzingo5@gmail.com>
2026-08-18 17:55:09 +0900

    calendar 구현중

--- 05_calendar/src/main.rs
+++ 05_calendar/src/main.rs
@@ -13,6 +13,11 @@ struct Schedule {
     start: NaiveDateTime,
     end: NaiveDateTime,
 }
+impl Schedule {
+    fn intersects(&self, other: &Schedule) -> bool {
+        self.start < other.end && other.start < self.end
+    }
+}
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 struct Calendar {
@@ -29,12 +34,23 @@ struct Cli {
 enum Commands {
     /// 일정 목록 표시
     List,
+    /// 일정 추가
+    Add {
+        subject: String,
+        start: NaiveDateTime,
+        end: NaiveDateTime,
+    },
 }
 
 fn main() {
     let options = Cli::parse();
     match options.command {
         Commands::List => show_list(),
+        Commands::Add {
+            subject,
+            start,
+            end,
+        } => add_schedule(subject, start, end),
     }
 }
 
@@ -50,5 +66,83 @@ fn show_list() {
             "{}\t{}\t{}\t{}",
             schedule.id, schedule.start, schedule.end, schedule.subject
         );
+    }
+}
+
+fn add_schedule(subject: String, start: NaiveDateTime, end: NaiveDateTime) {
+    let mut calendar: Calendar = {
+        let file = File::open(SCHEDULE_FILE).unwrap();
+        let reader = BufReader::new(file);
+        serde_json::from_reader(reader).unwrap()
+    };
+    let id = calendar.schedules.len() as u64;
+    let new_schedule = Schedule {
+        id,
+        subject,
+        start,
+        end,
+    };
+    for schedule in &calendar.schedules {
+        if schedule.intersects(&new_schedule) {
+            println!("오류: 일정이 중복됩니다.");
+            return;
+        }
+    }
+    calendar.schedules.push(new_schedule);
+    {
+        let file = File::create(SCHEDULE_FILE).unwrap();
+        let writer = BufWriter::new(file);
+        serde_json::to_writer(writer, &calendar).unwrap();
+    }
+    println!("일정을 추가했습니다.");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    fn naive_date_time(
+        year: i32,
+        month: u32,
+        day: u32,
+        hour: u32,
+        minute: u32,
+        second: u32,
+    ) -> NaiveDateTime {
+        chrono::NaiveDate::from_ymd_opt(year, month, day)
+            .unwrap()
+            .and_hms_opt(hour, minute, second)
+            .unwrap()
+    }
+
+    #[rstest]
+    #[case(18, 15, 19, 15, true)]
+    #[case(19, 45, 20, 45, true)]
+    #[case(18, 30, 20, 15, true)]
+    #[case(20, 15, 20, 45, false)]
+    #[case(18, 15, 18, 45, false)]
+    #[case(19, 15, 19, 45, true)]
+    fn test_schedule_intersects(
+        #[case] h0: u32,
+        #[case] m0: u32,
+        #[case] h1: u32,
+        #[case] m1: u32,
+        #[case] should_intersect: bool,
+    ) {
+        let schedule = Schedule {
+            id: 0,
+            subject: "기존 일정".to_string(),
+            start: naive_date_time(2025, 1, 1, h0, m0, 0),
+            end: naive_date_time(2025, 1, 1, h1, m1, 0),
+        };
+        let new_schedule = Schedule {
+            id: 999,
+            subject: "신규 일정".to_string(),
+            start: naive_date_time(2025, 1, 1, 19, 0, 0),
+            end: naive_date_time(2025, 1, 1, 20, 0, 0),
+        };
+
+        assert_eq!(should_intersect, schedule.intersects(&new_schedule));
     }
 }
